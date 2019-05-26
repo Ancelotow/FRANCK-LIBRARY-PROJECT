d7fc726b451eed237bbacc505b46fee98a9a0f07
Ancelotow <owen.ancelot.sio@gmail.com>
2019-05-26 10:28:06 +0200

    Activité de chargement

--- app/src/main/java/fr/ancelotow/franck_library_project/LoadActivity.java
+++ app/src/main/java/fr/ancelotow/franck_library_project/LoadActivity.java
@@ -1,13 +1,41 @@
 package fr.ancelotow.franck_library_project;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import fr.ancelotow.franck_library_project.Technique.Internet;
 
 public class LoadActivity extends AppCompatActivity {
+
+    private final static int TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(Internet.isConnectedInternet(LoadActivity.this))
+                {
+                    Intent i = new Intent(LoadActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(LoadActivity.this,
+                            "Vous n'êtes pas connecter à internet.",
+                            Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LoadActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        }, TIME_OUT);
     }
+
 }
---
+++ app/src/main/java/fr/ancelotow/franck_library_project/Technique/Internet.java
@@ -0,0 +1,28 @@
+package fr.ancelotow.franck_library_project.Technique;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
+
+public class Internet
+{
+    public static boolean isConnectedInternet(Activity activity)
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager)activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null)
+        {
+            State networkState = networkInfo.getState();
+            if (networkState.compareTo(State.CONNECTED) == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+
+}
+
