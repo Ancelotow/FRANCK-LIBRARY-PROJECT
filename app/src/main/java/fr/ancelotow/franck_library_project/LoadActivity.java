ae568fa6545ad191ba0e32ca4607511451aa7286
Ancelotow <owen.ancelot.sio@gmail.com>
2019-05-31 20:16:48 +0200

    Couleur de l'application

--- app/src/main/java/fr/ancelotow/franck_library_project/LoadActivity.java
+++ app/src/main/java/fr/ancelotow/franck_library_project/LoadActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.Toast;
 
 import fr.ancelotow.franck_library_project.Technique.Internet;
